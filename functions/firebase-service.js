44db0a37ea1850a992d603923ed3a35f58d35b78
Maria-Michail <maria.magdalena.michail@gmail.com>
2022-05-14 18:08:44 +0200

    Revert "initial commit"
    This reverts commit a5918cf8d97105bc72abaa20eb84337a5153247f.

--- functions/firebase-service.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-
-const credentials = require("./config/serviceAccountKey.json");
-
-admin.initializeApp({
-    credential:admin.credential.cert(credentials)
-});
-
-module.exports = admin;
